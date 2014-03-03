da28e394de08143bbc55df626f951e84d0d9cd48
Jonas <mr_jonas86@hotmail.com>
2014-03-03 10:58:31 +0100

    Lagt till en knapp

--- kd322b_testprojekt/src/kd322b_testprojekt/Test.java
+++ kd322b_testprojekt/src/kd322b_testprojekt/Test.java
@@ -6,6 +6,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
 
 public class Test extends JFrame {
 
@@ -35,8 +36,11 @@ public class Test extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnThisIsA = new JButton("This is a Button");
+		btnThisIsA.setBounds(50, 49, 339, 154);
+		contentPane.add(btnThisIsA);
 	}
-
 }
